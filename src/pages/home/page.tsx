12725ca7fa82d14e7fe1dd070acec90417e6b32f
Fraser <lydon@frasers-mbp.home>
2025-05-01 19:04:18 +0100

    init

---
+++ src/pages/home/page.tsx
@@ -0,0 +1,6 @@
+export default function App() {
+    return <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">SillyStack</h1>
+        <p className="text-2xl">The best way to learn new things</p>
+    </div>;
+  }
